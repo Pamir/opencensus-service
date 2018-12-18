d41f6802a2e139f6ec9c5814844042472f33a1b2
Emmanuel T Odeke <emmanuel@orijtech.com>
2018-12-19 02:18:09 +0300

    cmd/ocagent: started Stackdriver Metrics exporter
    First fitting of a Metrics exporter.

--- example/main.go
+++ example/main.go
@@ -22,6 +22,10 @@ import (
 	"time"
 
 	"contrib.go.opencensus.io/exporter/ocagent"
+
+	"go.opencensus.io/stats"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/tag"
 	"go.opencensus.io/trace"
 )
 
@@ -35,10 +39,41 @@ func main() {
 		DefaultSampler: trace.AlwaysSample(),
 	})
 
+	// Some stats
+        view.SetReportingPeriod(61 * time.Second)
+	keyClient, _ := tag.NewKey("client")
+	keyMethod, _ := tag.NewKey("method")
+
+        mLatencyMs := stats.Float64("latency", "The latency in milliseconds", "ms")
+        latencyView := &view.View{
+		Name:        "ocdemo/latency",
+		Description: "The various latencies",
+		Measure:     mLatencyMs,
+		Aggregation: view.Distribution(0, 10, 20, 50, 100, 200, 400, 600, 1000, 1600, 3200, 4800, 5600, 6400),
+                TagKeys: []tag.Key{keyClient, keyMethod},
+	}
+        countView := &view.View{
+		Name:        "ocdemo/process_counts",
+		Description: "The various counts",
+		Measure:     mLatencyMs,
+		Aggregation: view.Count(),
+                TagKeys: []tag.Key{keyClient, keyMethod},
+	}
+
+	// Some metrics
+	view.RegisterExporter(oce)
+	if err := view.Register(latencyView, countView); err != nil {
+		log.Fatalf("Failed to register views for metrics: %v", err)
+	}
+
+	ctx, _ := tag.New(context.Background(), tag.Insert(keyMethod, "repl"), tag.Insert(keyClient, "cli"))
 	rng := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for {
-		_, span := trace.StartSpan(context.Background(), "foo")
-		time.Sleep(time.Duration(rng.Int63n(1000)) * time.Millisecond)
+		startTime := time.Now()
+		_, span := trace.StartSpan(context.Background(), "Foo")
+		time.Sleep(time.Duration(rng.Int63n(3000)) * time.Millisecond)
 		span.End()
+		latencyMs := float64(time.Since(startTime)) / 1e6
+		stats.Record(ctx, mLatencyMs.M(latencyMs))
 	}
 }
